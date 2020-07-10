6afd9fc4320a7b16655b3467630e2b75d7d7b191
Tiankai Xie <xietiankai@gmail.com>
2020-07-09 20:53:00 -0700

    setup backend_builder script

---
+++ backend_builder.sh
@@ -0,0 +1,7 @@
+if [ ! "cached_data" ]; then
+  echo "Downloading data..."
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Rj0Flov3WTKwLygASM_42t57tJx3SYnr' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Rj0Flov3WTKwLygASM_42t57tJx3SYnr" -O cached_data.tar.gz && rm -rf /tmp/cookies.txt
+  tar -xzf cached_data.tar.gz
+fi
+
+gunicorn server:app -c ./gunicorn.conf.py
